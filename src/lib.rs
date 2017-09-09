0e588e01c5065e45ecbdbadf48a5600adf687422
Jonathan Creekmore <jonathan@thecreekmores.org>
2017-09-09 08:56:00 -0500

    extract helpers to a utils module

--- src/lib.rs
+++ src/lib.rs
@@ -56,40 +56,12 @@ extern crate nix;
 
 use nix::libc::c_int;
 use nix::poll;
-use std::cmp;
-use std::io::{Error, ErrorKind, Result};
+use std::io::Result;
 use std::io::{Read, Write};
 use std::os::unix::io::AsRawFd;
-use std::slice;
 use std::time::Duration;
 
-/// Convert from a duration into milliseconds as the c_int type that poll expects.
-/// If the duration exceeds the number of milliseconds that can fit into a c_int,
-/// saturate the time to the max_value of c_int.
-fn duration_to_ms(duration: Duration) -> c_int {
-    let secs = cmp::min(duration.as_secs(), c_int::max_value() as u64) as c_int;
-    let nanos = duration.subsec_nanos() as c_int;
-
-    secs.saturating_mul(1_000).saturating_add(nanos / 1_000_000)
-}
-
-/// Wait until `to_fd` receives the poll event from `events`, up to `timeout` length
-/// of time.
-fn wait_until_ready<R: AsRawFd>(timeout: &Option<c_int>,
-                                to_fd: &R,
-                                events: poll::EventFlags)
-                                -> Result<()> {
-    if let Some(timeout) = *timeout {
-        let mut pfd = poll::PollFd::new(to_fd.as_raw_fd(), events);
-        let mut s = unsafe { slice::from_raw_parts_mut(&mut pfd, 1) };
-
-        let retval = poll::poll(&mut s, timeout).map_err(|e| Error::new(ErrorKind::Other, e))?;
-        if retval == 0 {
-            return Err(Error::new(ErrorKind::TimedOut, "timed out waiting for fd to be ready"));
-        }
-    }
-    Ok(())
-}
+mod utils;
 
 /// The `TimeoutReader` struct adds read timeouts to any reader.
 ///
@@ -112,7 +84,7 @@ impl<H> Read for TimeoutReader<H>
     where H: Read + AsRawFd
 {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        wait_until_ready(&self.timeout, &self.handle, poll::POLLIN)?;
+        utils::wait_until_ready(&self.timeout, &self.handle, poll::POLLIN)?;
         self.handle.read(buf)
     }
 }
@@ -161,7 +133,7 @@ impl<H> TimeoutReader<H>
     /// ```
     pub fn new<T: Into<Option<Duration>>>(handle: H, timeout: T) -> TimeoutReader<H> {
         TimeoutReader {
-            timeout: timeout.into().map(duration_to_ms),
+            timeout: timeout.into().map(utils::duration_to_ms),
             handle: handle,
         }
     }
@@ -188,12 +160,12 @@ impl<H> Write for TimeoutWriter<H>
     where H: Write + AsRawFd
 {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        wait_until_ready(&self.timeout, &self.handle, poll::POLLOUT)?;
+        utils::wait_until_ready(&self.timeout, &self.handle, poll::POLLOUT)?;
         self.handle.write(buf)
     }
 
     fn flush(&mut self) -> Result<()> {
-        wait_until_ready(&self.timeout, &self.handle, poll::POLLOUT)?;
+        utils::wait_until_ready(&self.timeout, &self.handle, poll::POLLOUT)?;
         self.handle.flush()
     }
 }
@@ -242,7 +214,7 @@ impl<H> TimeoutWriter<H>
     /// ```
     pub fn new<T: Into<Option<Duration>>>(handle: H, timeout: T) -> TimeoutWriter<H> {
         TimeoutWriter {
-            timeout: timeout.into().map(duration_to_ms),
+            timeout: timeout.into().map(utils::duration_to_ms),
             handle: handle,
         }
     }
---
+++ src/utils.rs
@@ -0,0 +1,43 @@
+// Copyright 2017 Jonathan Creekmore
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use nix::libc::c_int;
+use nix::poll;
+use std::cmp;
+use std::io::{Error, ErrorKind, Result};
+use std::os::unix::io::AsRawFd;
+use std::slice;
+use std::time::Duration;
+
+/// Convert from a duration into milliseconds as the c_int type that poll expects.
+/// If the duration exceeds the number of milliseconds that can fit into a c_int,
+/// saturate the time to the max_value of c_int.
+pub fn duration_to_ms(duration: Duration) -> c_int {
+    let secs = cmp::min(duration.as_secs(), c_int::max_value() as u64) as c_int;
+    let nanos = duration.subsec_nanos() as c_int;
+
+    secs.saturating_mul(1_000).saturating_add(nanos / 1_000_000)
+}
+
+/// Wait until `to_fd` receives the poll event from `events`, up to `timeout` length
+/// of time.
+pub fn wait_until_ready<R: AsRawFd>(timeout: &Option<c_int>,
+                                    to_fd: &R,
+                                    events: poll::EventFlags)
+                                    -> Result<()> {
+    if let Some(timeout) = *timeout {
+        let mut pfd = poll::PollFd::new(to_fd.as_raw_fd(), events);
+        let mut s = unsafe { slice::from_raw_parts_mut(&mut pfd, 1) };
+
+        let retval = poll::poll(&mut s, timeout).map_err(|e| Error::new(ErrorKind::Other, e))?;
+        if retval == 0 {
+            return Err(Error::new(ErrorKind::TimedOut, "timed out waiting for fd to be ready"));
+        }
+    }
+    Ok(())
+}
